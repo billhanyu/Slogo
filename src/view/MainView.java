447bc13d3605065a89106bf6a11bcb150e8fca84
Bill Yu <han.yu@duke.edu>
2016-10-17 20:46:51 -0400

    minor UI adjustment

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -2,7 +2,6 @@ package view;
 
 import controller.Controller;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -50,11 +49,11 @@ public class MainView {
 		editor = new Editor(controller, 1000,0);
 		
 		VBox lefts = new VBox();
-		console = new Console(controller, 200,200);
-		UserControls controls = new UserControls(controller, 200, 200);
+		console = new Console(controller, 300,200);
+		UserControls controls = new UserControls(controller, 300, 200);
 		lefts.getChildren().addAll(console.getUI(), controls.getUI());
 				
-		environmentView = new EnvironmentView(controller, 300,500);
+		environmentView = new EnvironmentView(controller, 200,500);
 		root.setCenter(canvas.getUI());
 		root.setBottom(editor.getUI());
 		root.setLeft(lefts);
